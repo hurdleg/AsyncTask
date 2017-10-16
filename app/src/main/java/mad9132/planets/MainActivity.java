69345a7f7ac71ddeff2b6e7653a64d639366d6d7
Gerald Hurdle <hurdleg@algonquincollege.com>
2017-10-16 15:20:49 -0400

    Initial commit.

--- app/src/main/java/mad9132/planets/MainActivity.java
+++ app/src/main/java/mad9132/planets/MainActivity.java
@@ -38,11 +38,27 @@ public class MainActivity extends Activity {
 
     private class MyAsyncTask extends AsyncTask<String, String, Void> {
 
+        /**
+         * Invoked on the UI thread before the task is executed.
+         * This step is normally used to setup the task.
+         *
+         */
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
         }
 
+        /**
+         * Invoked on the background thread immediately after onPreExecute() finishes executing.
+         * This step is used to peform background computation that can take a long time.
+         * The parameters of the asynchronous task are passed to this step.
+         * The result of the computation must be returned by this step and will be passed back to
+         * the last step.
+         * This step can also use publishProgress(Progress) to publish one or more units of progress.
+         * These values are published on the UI thread, in the onProgressUpdate(Progress) step.
+         * @param strings
+         * @return Void
+         */
         @Override
         protected Void doInBackground(String... strings) {
             for (String string: strings) {
@@ -57,12 +73,23 @@ public class MainActivity extends Activity {
             return null;
         }
 
+        /**
+         * Invoked on the UI thread after a call to publishProgress(Progress).
+         * The timing of the execution is undefined. This method is used to display any form of
+         * progress in the UI while the background computation is still executing.
+         * @param values
+         */
         @Override
         protected void onProgressUpdate(String... values) {
             super.onProgressUpdate(values);
             output.append(values[0] + "\n");
         }
 
+        /**
+         * Invoked on the UI thread after the background computation finishes.
+         * The result of the background computation is passed to this step as a parameter.
+         * @param aVoid
+         */
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
